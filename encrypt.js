14a418d1dda3b40f0e891df00688e9b554dfc5b3
fannyhasbi <fnyhsbi@gmail.com>
2019-03-02 22:56:02 +0700

    First functionally encryption

--- encrypt.js
+++ encrypt.js
@@ -1,10 +1,40 @@
-const message = "HASBI";
-const pattern = "HAZ";
+"use strict"
+
+const btoa = require('btoa');
+
+const MESSAGE = "HASBI";
+const KEY = "HAZ";
+
+var encrypt = (message, pattern) => {
+  var result = [];
+
+  /**
+    * ASCII CAPITAL LETTER : 65 - 90
+    */
 
 
-for(i = 0; i < message.length; i++){
-  a = message.charCodeAt(i);
-  b = String.fromCharCode(a);
+  for(let i = 0; i < message.length; i++){
+    let a = message.charCodeAt(i);
+    // let b = String.fromCharCode(a);
 
-  console.log(a, b);
+    let shifter = KEY.charCodeAt(Math.floor(i % KEY.length));
+
+    let xor = a ^ shifter;
+
+    result.push(xor);
+  }
+
+  return result;
 }
+
+var encode = (data) => {
+  // console.log(btoa(data));
+  // console.log(data);
+
+  return btoa(data);
+}
+
+let encrypted = encrypt(MESSAGE, KEY);
+let result = encode(encrypted);
+
+console.log(result);
