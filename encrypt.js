0c0e6bb4f1a09953de1ce292258d5df0063e2ec1
fannyhasbi <fnyhsbi@gmail.com>
2019-03-03 00:44:58 +0700

    Cover it into an object

--- encrypt.js
+++ encrypt.js
@@ -1,40 +1,44 @@
+/**
+  * @author Fanny Hasbi
+  * @website https://fannyhasbi.id
+  */
+
 "use strict"
 
 const atob = require('atob');
 const btoa = require('btoa');
-const MESSAGE = "HASBI";
-const KEY = "HOKBEN";
 
-var encrypt = (message, key) => {
-  var result = [];
-  /**
-    * ASCII CAPITAL LETTER : 65 - 90
-    */
+const MESSAGE = "FANNYHASBI";
+const KEY = "FUN";
 
-  for(let i = 0; i < message.length; i++){
-    let a = message.charCodeAt(i);
-    let shifter = key.charCodeAt(Math.floor(i % key.length));
-    result.push(a^shifter);
+const FunEncrypt = {
+  encrypt: (message, key) => {
+    var result = [];
+    for(let i = 0; i < message.length; i++){
+      let a = message.charCodeAt(i);
+      let shifter = key.charCodeAt(Math.floor(i % key.length));
+      result.push(a^shifter);
+    }
+    return btoa(result);
+  },
+
+  decrypt: (secret, key) => {
+    secret = atob(secret).split(",");
+    var result = [];
+
+    secret.forEach((e, i) => {
+      let shifter = key.charCodeAt(Math.floor(i % key.length));
+      let b = String.fromCharCode(e ^ shifter);
+
+      result.push(b);
+    });
+
+    return result.join("");
   }
-  return btoa(result);
 }
 
-var decrypt = (secret, key) => {
-  var secret = atob(secret).split(",");
-  var result = [];
+let secret = FunEncrypt.encrypt(MESSAGE, KEY);
+let origin = FunEncrypt.decrypt(secret, KEY);
 
-  secret.forEach((e, i) => {
-    let shifter = key.charCodeAt(Math.floor(i % key.length));
-    let b = String.fromCharCode(e ^ shifter);
-
-    result.push(b);
-  });
-
-  return result.join("");
-}
-
-let secret = encrypt(MESSAGE, KEY);
-let origin = decrypt(secret, KEY);
-
-// console.log(secret);
+console.log(secret);
 console.log(origin);
