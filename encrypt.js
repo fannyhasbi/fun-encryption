617192d3af44ac8762b7b8374e56cf1b461897dd
fannyhasbi <fnyhsbi@gmail.com>
2019-03-02 23:03:28 +0700

    Refactor code

--- encrypt.js
+++ encrypt.js
@@ -1,40 +1,23 @@
 "use strict"
 
 const btoa = require('btoa');
-
 const MESSAGE = "HASBI";
-const KEY = "HAZ";
+const KEY = "HOKBEN";
 
 var encrypt = (message, pattern) => {
   var result = [];
-
   /**
     * ASCII CAPITAL LETTER : 65 - 90
     */
 
-
   for(let i = 0; i < message.length; i++){
     let a = message.charCodeAt(i);
-    // let b = String.fromCharCode(a);
-
     let shifter = KEY.charCodeAt(Math.floor(i % KEY.length));
-
-    let xor = a ^ shifter;
-
-    result.push(xor);
+    result.push(a^shifter);
   }
-
-  return result;
+  return btoa(result);
 }
 
-var encode = (data) => {
-  // console.log(btoa(data));
-  // console.log(data);
+let secret = encrypt(MESSAGE, KEY);
 
-  return btoa(data);
-}
-
-let encrypted = encrypt(MESSAGE, KEY);
-let result = encode(encrypted);
-
-console.log(result);
+console.log(secret);
